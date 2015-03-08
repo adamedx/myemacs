0987520f0490e3220ab6b38fb606b2396e2e9751
adamedx <adamed@getchef.com>
2015-03-08 14:55:51 -0700

    Bump version number to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'adamed@opscode.com'
 license          'Apache 2.0'
 description      'Configures Emacs'
 long_description 'Configures Emacs'
-version          '0.1.0'
+version          '0.2.0'
 
 depends          'git'
 
